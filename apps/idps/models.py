d23704a0f7774d7f646a0a58bdac85205e5edff7
Клавдия Дунаева <klavam@yandex.ru>
2024-01-21 14:45:07 +0700

    создание модели ИПР, админки.

--- apps/idps/models.py
+++ apps/idps/models.py
@@ -1,6 +1,18 @@
 from django.conf import settings
+from django.core.exceptions import ValidationError
 from django.db import models
-from django.db.models import UniqueConstraint
+from django.db.models import CheckConstraint, UniqueConstraint
+
+from apps.users.models import ChiefEmployee, Role, User, UserRole
+
+
+class Status(models.TextChoices):
+    """Варианты статусов."""
+
+    IN_PROGRESS = ("In progress", "В работе")
+    WORK_DONE = ("Work done", "Выполнен")
+    NOT_DONE = ("Not done", "Не выполнен")
+    EMPTY = ("Empty", "Отсутствует")
 
 
 class Task(models.Model):
@@ -14,7 +26,10 @@ class Task(models.Model):
     class Meta:
         verbose_name = "Задача"
         verbose_name_plural = "Задачи"
-        ordering = ("created_at",)
+        ordering = (
+            "text",
+            "created_at",
+        )
 
     def __str__(self):
         return self.text
@@ -69,3 +84,122 @@ class GoalTask(models.Model):
 
     def __str__(self):
         return f"Цель {self.goal.title} включает задачу {self.tasks.text}"
+
+
+class Ipr(models.Model):
+    title = models.CharField(
+        max_length=settings.FIELD_TITLE_LENGTH, verbose_name="Наименование ИПР"
+    )
+    employee = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name="Сотрудник",
+        related_name="my_ipr",
+    )
+    chief = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name="Руководитель",
+        related_name="ipr_for_employee",
+    )
+    goals = models.ManyToManyField(
+        Goal, through="GoalForIpr", verbose_name="Цели"
+    )
+    status = models.CharField(
+        max_length=max([len(status) for status in Status]),
+        choices=Status,
+        default=Status.IN_PROGRESS,
+        verbose_name="Статус ИПР",
+    )
+    created_at = models.DateTimeField(
+        verbose_name="Дата создания", auto_now_add=True, db_index=True
+    )
+    finished_at = models.DateTimeField(
+        verbose_name="Дата закрытия ИПР", blank=True, null=True, db_index=True
+    )
+
+    class Meta:
+        verbose_name = "ИПР"
+        verbose_name_plural = "ИПРы"
+        constraints = [
+            UniqueConstraint(
+                fields=["title", "chief", "employee"],
+                name="unique_ipr_for_employee",
+            ),
+            CheckConstraint(
+                name="self_follow",
+                check=~models.Q(chief=models.F("employee")),
+            ),
+        ]
+        ordering = ("created_at", "employee", "chief", "status")
+
+    def __str__(self):
+        return (
+            f"ИПР:{self.title},\n Создатель:{self.chief}, \n"
+            f"Исполнитель:{self.employee}, \n Статус:{self.status}"
+        )
+
+    def clean(self):
+        if self.employee == self.chief:
+            raise ValidationError("Вы не можете назначить ИПР самому себе")
+        if not UserRole.objects.filter(
+            user=self.chief, role=Role.CHIEF
+        ).exists():
+            raise ValidationError(
+                "Руководитель должен иметь соответствующую роль!"
+            )
+        if not UserRole.objects.filter(
+            user=self.employee, role=Role.EMPLOYEE
+        ).exists():
+            raise ValidationError(
+                "Сотрудник должен иметь соответствующую роль!"
+            )
+        if not ChiefEmployee.objects.filter(
+            chief=self.chief, employee=self.employee
+        ).exists():
+            raise ValidationError("Возможно это не ваш сотрудник?")
+
+
+class GoalForIpr(models.Model):
+    """Модель цели со статусом для конкретного ИПР"""
+
+    goal = models.ForeignKey(
+        Goal,
+        blank=False,
+        null=False,
+        on_delete=models.CASCADE,
+        verbose_name="Цель",
+        related_name="ipr_goals",
+    )
+    status = models.CharField(
+        max_length=max([len(status) for status in Status]),
+        choices=Status,
+        default=Status.IN_PROGRESS,
+        verbose_name="Статус Цели",
+    )
+    created_at = models.DateTimeField(
+        verbose_name="Дата создания", auto_now_add=True, db_index=True
+    )
+    deadline = models.DateTimeField(
+        verbose_name="Дата дедлайна", db_index=True
+    )
+    ipr = models.ForeignKey(
+        Ipr,
+        on_delete=models.CASCADE,
+        related_name="ipr_goals",
+        verbose_name="ИПР",
+    )
+    finished_at = models.DateTimeField(
+        verbose_name="Дата закрытия цели", blank=True, null=True, db_index=True
+    )
+
+    class Meta:
+        verbose_name = "Цель ИПР"
+        verbose_name_plural = "Цели для ИПР"
+        ordering = ("created_at", "ipr__title", "status", "deadline")
+
+    def __str__(self):
+        return (
+            f"ИПР:{self.ipr}, Цель:{self.goal},\n"
+            f"Статус: {self.status},\n Дедлайн: {self.deadline}"
+        )
