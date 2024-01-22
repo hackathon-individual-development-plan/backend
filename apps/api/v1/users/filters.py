645bd03530e686dfef1ce06bd6d03dd541989352
Ольга Ротбардт <knyajna.tarakanova@gmail.com>
2024-01-22 05:07:10 +0400

    fix filters

--- apps/api/v1/users/filters.py
+++ apps/api/v1/users/filters.py
@@ -1,15 +1,13 @@
 from django_filters.rest_framework import CharFilter, FilterSet
 
-from apps.users.models import User
+from apps.users.models import ChiefEmployee
 
 
 class EmployeeWithoutIdpFilter(FilterSet):
     """Поиск по частичному вхождению в начале ФИО."""
 
-    name = CharFilter(lookup_expr="istartswith")
+    fio = CharFilter(field_name="employee__fio", lookup_expr="istartswith")
 
     class Meta:
-        model = User
-        fields = [
-            "fio",
-        ]
+        model = ChiefEmployee
+        fields = ["fio"]
