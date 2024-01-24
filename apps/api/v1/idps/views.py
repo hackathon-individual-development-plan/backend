dabb9e56722fadda1b70d803e04b4c0528678853
Клавдия Дунаева <klavam@yandex.ru>
2024-01-24 20:10:08 +0700

    сделала PATCH ипр-ов, изменила вьюсет Idp

--- apps/api/v1/idps/views.py
+++ apps/api/v1/idps/views.py
@@ -1,23 +1,17 @@
-from rest_framework import viewsets
+from django.db.models import OuterRef
 
-from apps.api.v1.idps.serializers import IdpSerializer  # , PostIdpSerializer
-from apps.idps.models import Idp
+from apps.api.v1.idps.mixinview import CreateRetrieveViewSet
+from apps.api.v1.idps.serializers import IdpSerializer, PostIdpSerializer
+from apps.idps.models import GoalTask, Idp
 
 
-class IdpViewSet(viewsets.ModelViewSet):
-    queryset = Idp.objects.all()
+class IdpViewSet(CreateRetrieveViewSet):
+    tasks_for_goal = GoalTask.objects.filter(goal_id=OuterRef("goal_id"))
+    queryset = Idp.objects.all().prefetch_related("goals")
     # permission_classes = (IsChief,) TODO написать пермишн
     ordering_fields = ["-created_at"]
 
     def get_serializer_class(self):
-        # if self.request.method in ["POST", "PATCH"]:
-        #     return PostIdpSerializer
+        if self.request.method in ["POST", "PATCH"]:
+            return PostIdpSerializer
         return IdpSerializer
-
-
-# class IngredientsViewSet(viewsets.ReadOnlyModelViewSet):
-#     queryset = Ingredient.objects.all()
-#     pagination_class = None
-#     serializer_class = IngredientsSerializer
-#     filter_backends = (DjangoFilterBackend,)
-#     filterset_class = IngredientsFilter
