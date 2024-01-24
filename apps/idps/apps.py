c855d57faf955623da23c13d44c60013526e09a5
NV_kovalev <anit200@yandex.ru>
2024-01-24 18:56:26 +0300

    Удалены лишние файлы в процессе реструктуризации

--- apps/idps/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class IdpsConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "apps.idps"
-    verbose_name = "Индивидуальные Планы Развития"
